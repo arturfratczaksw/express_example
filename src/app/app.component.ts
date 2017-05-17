64250c2437cb1263392a06e295237ebf03d39ac8
AF <artur.fratczak@smoothwall.net>
2017-05-17 10:43:44 +0100

    Add server attempt 2?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'does app work?';
+  title = 'does app work? yes it does!';
 }
